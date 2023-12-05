30f857ff0818ab6295ac5f61ea81cee9f4cc4a9d
phxofc <phdfrocha99@gmail.com>
2023-12-05 18:19:48 -0300

    create project

---
+++ src/main/java/br/com/pedrodev/junit5mockitoapirest/JUnit5MockitoApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrodev.junit5mockitoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5MockitoApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnit5MockitoApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pedrodev/junit5mockitoapirest/JUnit5MockitoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedrodev.junit5mockitoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5MockitoApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
